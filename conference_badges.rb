ef8d41d2204f354d3dd1ed12a3897b8fbd423e56
Phil Hartley <philh725@gmail.com>
2018-10-31 20:07:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,9 @@ end
 def batch_badge_creator(array)
   output = []
   for i in (0..array.size - 1)
-    output.push("Hello, my name is #{array[i]}.")
+    #output.push("Hello, my name is #{array[i]}.")
+    output.push( badge_maker(array[i]) )
   end
   output
 end
 
-puts badge_maker("phil")
